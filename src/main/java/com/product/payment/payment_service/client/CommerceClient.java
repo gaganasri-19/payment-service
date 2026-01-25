f465fe52835d9d85d96c2e454539e7c4469d1897
Gaganasri <sripriyapvasista@gmail.com>
2026-01-26 01:08:22 +0530

    add resilience

--- src/main/java/com/product/payment/payment_service/client/CommerceClient.java
+++ src/main/java/com/product/payment/payment_service/client/CommerceClient.java
@@ -1,8 +1,13 @@
 package com.product.payment.payment_service.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 
 @Component
 public class CommerceClient {
@@ -10,10 +15,13 @@ public class CommerceClient {
     private final RestTemplate restTemplate = new RestTemplate();
     private final String BASE_URL;
 
+    private static final Logger log = LoggerFactory.getLogger(CommerceClient.class);
     private CommerceClient(@Value("${commerce.service.url}") String commerceServiceUrl) {
         this.BASE_URL = commerceServiceUrl;
     }
 
+    @Retry(name = "commerceService")
+    @CircuitBreaker(name = "commerceService", fallbackMethod = "fallback")
     public void notifyPaymentSuccess(Long orderId) {
         restTemplate.postForEntity(
             BASE_URL + "/api/internal/payments/success/" + orderId,
@@ -22,12 +30,18 @@ public class CommerceClient {
         );
     }
 
+    @Retry(name = "commerceService")
+    @CircuitBreaker(name = "commerceService", fallbackMethod = "fallback")
     public void notifyPaymentFailure(Long orderId) {
         restTemplate.postForEntity(
             BASE_URL + "/api/internal/payments/failed/" + orderId,
             null,
             Void.class
         );
+    }
+
+    public void fallback(Long orderId, Throwable ex) {
+    System.out.println("Fallback triggered for order " + orderId);
     }
 }
 
