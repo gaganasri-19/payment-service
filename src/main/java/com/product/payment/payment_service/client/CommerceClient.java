12cf382c4c258a692214d93d8eb1a33fe2072d13
Gaganasri <sripriyapvasista@gmail.com>
2026-01-26 21:49:04 +0530

    code refactor

--- src/main/java/com/product/payment/payment_service/client/CommerceClient.java
+++ src/main/java/com/product/payment/payment_service/client/CommerceClient.java
@@ -1,7 +1,5 @@
 package com.product.payment.payment_service.client;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
